2387418d18024fc7fc2c1f12faee9309e29324ef
Eiji Masaki <masaki@linkode.co.jp>
2020-02-29 21:57:03 +0900

    TimeEntry出力

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module toggl_sample01
 
 go 1.13
 
-require github.com/jason0x43/go-toggl v0.0.0-20191006134724-98db1b5443ff // indirect
+require (
+	github.com/ahmetb/go-linq v3.0.0+incompatible
+	github.com/jason0x43/go-toggl v0.0.0-20191006134724-98db1b5443ff
+)
--- main.go
+++ main.go
@@ -11,9 +11,12 @@ The API token can be retrieved from a user's account information page at toggl.c
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"os"
 	"github.com/jason0x43/go-toggl"
+	. "github.com/ahmetb/go-linq"
+	"fmt"
+	"time"
 )
 
 func main() {
@@ -21,6 +24,8 @@ func main() {
 		println("usage:", os.Args[0], "API_TOKEN")
 		return
 	}
+
+	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
 
 	session := toggl.OpenSession(os.Args[1])
 
@@ -30,6 +35,39 @@ func main() {
 		return
 	}
 
-	data, err := json.MarshalIndent(&account, "", "    ")
-	println("account:", string(data))
+	//projectMap := map[int]string{}
+	projectMap := makeProjectMap(account)
+/*
+	From(account.Data.Projects).ToMapByT(&projectMap,
+        func(p toggl.Project) int { return p.ID },
+        func(p toggl.Project) string { return p.Name },
+    )
+*/
+
+	From(account.Data.TimeEntries).ForEachT(func(te toggl.TimeEntry) {	
+		//data, _ := json.MarshalIndent(te.(toggl.TimeEntry).Description, "", "    ")
+		//println("account:", string(data))
+
+		start := te.Start.In(jst)
+		stop := te.Stop.In(jst)
+		duration := time.Duration(te.Duration) * time.Second
+		fmt.Printf("- [%s] %02d:%02d-%02d:%02d %v %v\n", fmtDurationHHMM(duration), start.Hour(), start.Minute(), stop.Hour(), stop.Minute(), projectMap[te.Pid], te.Description)
+	})
+}
+
+func makeProjectMap(account toggl.Account) map[int]string {
+	projectMap := map[int]string{}
+	From(account.Data.Projects).ToMapByT(&projectMap,
+        func(p toggl.Project) int { return p.ID },
+        func(p toggl.Project) string { return p.Name },
+	)
+	return projectMap
+}
+
+func fmtDurationHHMM(d time.Duration) string {
+    d = d.Round(time.Minute)
+    h := d / time.Hour
+    d -= h * time.Hour
+    m := d / time.Minute
+    return fmt.Sprintf("%02d:%02d", h, m)
 }
